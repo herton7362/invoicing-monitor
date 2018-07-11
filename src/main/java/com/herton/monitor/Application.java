553950ac5176f2f512744a948f8ac43a52ebed9e
herton7362 <he.tang@everydayratings.com>
2018-07-11 13:13:00 +0800

    init

---
+++ src/main/java/com/herton/monitor/Application.java
@@ -0,0 +1,13 @@
+package com.herton.monitor;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
